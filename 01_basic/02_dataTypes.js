ff2b7f68fff76d735b279a851308c222c5c65f87
Zaid <khanzaid63945@gmail.com>
2026-08-15 03:18:49 +0000

    Conversion going

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,39 @@
+"use strict"; //using new js
+
+// console.log( 3 +3);
+
+// let name = "zaid" //string
+// let age = 18 //Number
+// let isLoggedIn  = true //boolean
+
+//Number => 2^53 range
+//bigInt 
+//string => ""
+//boolean => true/false
+//null => standalone value
+// undefined 
+//symbol => to find uniqueness
+//premitive data type
+
+
+//object
+
+// console.log(typeof undefined); //undefined
+// console.log(typeof null); //object
+
+//conversion and Operation
+
+let score = "33"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof (valueInNumber));
+
+// "33" => 33
+
+let isLoggedIn = 0
+
+let booeanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booeanIsLoggedIn);
