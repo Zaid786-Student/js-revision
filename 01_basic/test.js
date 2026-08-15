ad37dcfd8c0a351b0cf3a3e906178c0009d0ee0c
Zaid <khanzaid63945@gmail.com>
2026-08-15 02:17:27 +0000

    learnign basic variables

---
+++ 01_basic/test.js
@@ -0,0 +1,15 @@
+const accountId = 144552
+let accountEmail = "Zaid@gmail.com"
+var accountPassword = "123456"
+accountCity = "Lucknow"
+
+// accountId = 2
+accountEmail = "Hello@gmail.com"
+accountPassword = "33543"
+accountCity = "Kanpur"
+
+// console.log(accountEmail);
+// console.log(accountId);
+// console.log(accountPassword);
+// console.log(accountCity);
+console.table([accountEmail, accountId, accountPassword, accountCity])
