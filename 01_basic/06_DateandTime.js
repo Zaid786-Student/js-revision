07a9c478e6a79383647a3c047ff541e671a1cc0e
Zaid <khanzaid63945@gmail.com>
2026-08-16 10:17:17 +0000

    array

---
+++ 01_basic/06_DateandTime.js
@@ -0,0 +1,13 @@
+// let myDate = new Date("2006-01-06")
+let myDate = new Date("06-11-2006")
+
+// console.log(myDate.toLocaleDateString());
+
+// console.log(myDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+
+console.log(newDate);
+console.log(newDate.getDay());
+console.log(newDate.getMonth());
