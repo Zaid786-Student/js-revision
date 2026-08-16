123ceb9c69c7e62c2747c318760ec2fdd9e64572
Zaid <khanzaid63945@gmail.com>
2026-08-16 03:48:53 +0000

    nums and maths

--- 01_basic/05_nums_and_maths.js
+++ 01_basic/05_nums_and_maths.js
@@ -1,1 +1,21 @@
 const score = 400
+
+const balance = new Number(1000)
+// console.log(balance.toLocaleString());
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
+// console.log(balance.toExponential(2));
+
+const hundreds = 1000000
+
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//*****************************************MATHS**********************************/
+
+console.log(Math.SQRT2);
+console.log(Math.round(4.5));
+console.log(Math.pow(2, 3));
+console.log(Math.floor(Math.random()*10)+1);
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
